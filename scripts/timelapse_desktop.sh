64006dffdee5d2838a91e09b7d3602f7b5fde023
booyaa <booyaabooyaabooyaa@gmail.com>
2015-09-16 08:02:26 +0100

    forgot to stick datestamp in loop

--- scripts/timelapse_desktop.sh
+++ scripts/timelapse_desktop.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-DATE_STAMP=$(date +"%Y%m%d-%H%M")
-SAVE_TO=~/Desktop/dockerhackday/
-FILE_NAME=${SAVE_TO}${DATE_STAMP}.png
 SCREENCAP_NO_SOUND=-x
 SCREENCAP_NO_DELAY=-T0
 SCREENCAP_OPTS="${SCREENCAP_NO_SOUND} ${SCREENCAP_NO_DELAY}"
@@ -10,6 +7,10 @@ SLEEP_IN_SECONDS=60
 
 while true
 do
+  # who has two thumbs and forgot to put the date string eval in the loop?
+  DATE_STAMP=$(date +"%Y%m%d-%H%M")
+  SAVE_TO=~/Desktop/dockerhackday/
+  FILE_NAME=${SAVE_TO}${DATE_STAMP}.png
   screencapture ${SCREENCAP_OPTS} ${FILE_NAME}
   echo sleeping for ${SLEEP_IN_SECONDS} seconds
   sleep ${SLEEP_IN_SECONDS}
